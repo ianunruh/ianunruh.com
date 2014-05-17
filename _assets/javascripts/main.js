20c09365e4bb00073e8d99bc61c4304eaad11c73
Ian Unruh <ianunruh@gmail.com>
2014-05-16 23:40:00 -0500

    Added auto-linking for post images

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,3 +2,12 @@
 //= require bootstrap
 //= require ga
 //= require_self
+
+$(document).ready(function() {
+
+  $(".post img").wrap(function() {
+    var $this = $(this);
+    return '<a href="' + $this.attr('src') + '">' + $this.text() + '</a>';
+  });
+
+});
